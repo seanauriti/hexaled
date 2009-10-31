df561f18b022b64e1b2fb64b9192185526379ae1
rmd6502 <rmd6502@4e552afc-b617-11de-81bc-59952d0011f9>
2009-10-31 21:29:15 +0000

    do rest of web control

---
+++ update.php
@@ -0,0 +1,20 @@
+<?php
+    header('Content-type: text/xml');
+
+    include "php_serial.class.php";
+    include "config.php";
+    $f = new phpSerial;
+    $f->deviceSet($SERIAL_PORT);
+    $f->confBaudRate(115200);
+    $f->deviceOpen();
+
+    # See if we got here from a form submission
+    if ($_REQUEST["cmd"])
+    {
+        $f->sendMessage($_REQUEST["cmd"]);
+    }
+    $f->sendMessage("Q");
+    $currentstate = $f->readPort();
+?><status>
+    <decimal><?= $currentstate ?></decimal>
+</status>
